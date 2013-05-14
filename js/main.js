3520f9ac40207e878c15270f21b0a75a424ee43c
= <=>
2013-05-14 19:40:07 +0200

    added about & tweets

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+jQuery(function ($) {
+	var $window = $(window);
+	$window.resize(resizeVideo);
 
+	var $video = $('#video'),
+		$top = $('#top'),
+		VIDWIDTH = 1920,
+		VIDHEIGHT = 668,
+		SCALE = VIDHEIGHT / VIDWIDTH;
+
+	function resizeVideo() {
+		$video.css({
+			height: SCALE * $window.width() + 'px',
+			marginTop: $top.height() + 'px'
+		});
+	}
+	resizeVideo();
+
+
+	var $buddha = $('#buddha'), startTop = parseInt($buddha.css('top'), 10);
+	$window.on('scroll', function () {
+		$buddha.css({
+			top: (startTop - $window.scrollTop()/2) + 'px'
+		});
+	});
+
+
+	$('#tweet-holder').tweets({username: 'idleworks'}, function (data) {
+		console.log(data);
+		console.log('done');
+	});
+});
